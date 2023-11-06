8282b63b4b95767a2ee998a5e7ba0a35bb34f06d
vinay kumar <vkumar8192449@gmail.com>
2023-11-06 14:40:31 +0530

    warning fixes

--- src/components/Gallery1.jsx
+++ src/components/Gallery1.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React from "react";
 import "../styles/Gallery1.css";
 
 const Gallery1 = () => {
   return (
     <section className="hero-section">
       <div className="card-grid">
-        <a className="card" href="#">
-          <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)'}}></div>
+        <a className="card" href="/">
+          <div
+            className="card__background"
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)",
+            }}
+          ></div>
           <div className="card__content">
             <p className="card__category">Category</p>
             <h3 className="card__heading">Example Card Heading</h3>
           </div>
         </a>
-        <a className="card" href="#">
-          <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+        <a className="card" href="/">
+          <div
+            className="card__background"
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)",
+            }}
+          ></div>
           <div className="card__content">
             <p className="card__category">Category</p>
             <h3 className="card__heading">Example Card Heading</h3>
           </div>
         </a>
-        <a className="card" href="#">
-          <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+        <a className="card" href="/">
+          <div
+            className="card__background"
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)",
+            }}
+          ></div>
           <div className="card__content">
             <p className="card__category">Category</p>
             <h3 className="card__heading">Example Card Heading</h3>
           </div>
         </a>
-        <a className="card" href="#">
-          <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+        <a className="card" href="/">
+          <div
+            className="card__background"
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)",
+            }}
+          ></div>
           <div className="card__content">
             <p className="card__category">Category</p>
             <h3 className="card__heading">Example Card Heading</h3>
